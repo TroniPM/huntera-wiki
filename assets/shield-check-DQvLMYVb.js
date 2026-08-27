a5e1be266abc28d055c9b3d9955a856092833953
Paulo Mateus <paulomatew@gmail.com>
2026-08-27 20:51:14 -0300

    1.4.0

--- assets/shield-check-DQvLMYVb.js
+++ assets/shield-check-DQvLMYVb.js
@@ -1,4 +1,4 @@
-import{E as e}from"./index-uUu-pMr8.js";/**
+import{E as e}from"./index-DNx0CAmG.js";/**
  * @license lucide-vue-next v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
